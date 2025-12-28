b6073ff57d549a9be8e67e869e4c22eb544500f3
menik1126 <menik1126@gmail.com>
2025-12-28 02:18:35 -0500

    add scripts for wmt and ppl

---
+++ scripts/scripts_wmt/metrics.sh
@@ -0,0 +1,16 @@
+
+results_dir=""
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --results_dir) results_dir="$2"; shift 2;; 
+    --new_method) new_method="$2"; shift 2;;
+    --switch) switch="$2"; shift 2;;
+    *) echo "Unknown option: $1"; exit 1;;
+  esac
+done
+
+python3 eval.py \
+    --results_dir ${results_dir} \
+    --new_method ${new_method} \
+    --switch ${switch}
